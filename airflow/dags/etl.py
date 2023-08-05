3a040866639e61c06138d4635e54cff7c446d712
Rudraksh Karpe <rudraksh.karpe@gmail.com>
2023-08-05 09:02:58 +0530

    feat: DAG perform advanced data transformation

--- airflow/dags/etl.py
+++ airflow/dags/etl.py
@@ -49,3 +49,62 @@ with DAG(
             product[k] = random.choice(v)
         product["product_id"] = -1
         return product
+    
+     def transform():
+        prdcts=pd.read_csv('/opt/airflow/datasets/products.csv')
+        baskets=pd.read_csv("/opt/airflow/datasets/basket_details.csv")
+        customers=pd.read_csv("/opt/airflow/datasets/customer_details.csv")
+
+        columns_values={}
+        for c in prdcts.columns:
+            if c!='product_id':
+                columns_values[c]=prdcts[c].unique()
+        generated={}
+        number_products=len(baskets.product_id.unique())
+        for i in range(number_products-prdcts.shape[0]):
+            generated[i]=generate_product(columns_values)
+        
+        gennerated_df=pd.DataFrame(generated).T
+        prdcts=pd.concat([prdcts,gennerated_df], ignore_index=True)
+        prdcts.product_id=baskets.product_id.unique()
+        prdcts['created_on']=prdcts['created_on'].map(lambda x: x.replace("T",' ')[:-6])
+        prdcts.to_csv("/opt/airflow/datasets/products-generated.csv",index=False)
+
+        customers["sex"]=customers["sex"].map(lambda x : "UKNOWN" if ((x.lower().strip()!="female") and (x.lower().strip()!='male')) else x.lower().strip() )
+        customers["customer_age"]=customers["customer_age"].map(lambda x : None if (x > 100) else int(abs(x)))
+        customers["customer_age"]=customers["customer_age"].fillna(customers["customer_age"].median())
+        customers["customer_age"]=customers["customer_age"].astype(int)
+        try:
+            assert customers["tenure"].isna().sum()==0
+            assert customers["customer_age"].isna().sum()==0
+            assert customers["sex"].isna().sum()==0
+            assert customers["customer_id"].isna().sum()==0
+            assert all(customers["sex"].unique()== ['male', 'female', 'UKNOWN'])
+            assert all(customers["customer_age"].unique()>0) and all(customers["customer_age"].unique()< 101)
+
+            customers.to_csv("/opt/airflow/datasets/customers-transformed.csv",index=False)
+
+            assert baskets["customer_id"].isna().sum()==0
+            assert baskets["product_id"].isna().sum()==0
+            assert baskets["basket_date"].isna().sum()==0
+            assert baskets["basket_count"].isna().sum()==0
+            assert all(baskets["basket_count"].unique()>0)
+
+        except AssertionError:
+            _, _, tb = sys.exc_info()
+            traceback.print_tb(tb)
+            tb_info = traceback.extract_tb(tb)
+            filename, line, func, text = tb_info[-1]
+            raise AirflowFailException('An error occurred on line {} in statement {}'.format(line, text))
+
+
+    transform_task=PythonOperator(
+        task_id="transform",
+        python_callable=transform,
+        dag=dag
+    )
+
+    def df_to_rows(df):
+        return df.values.tolist()
+
+
