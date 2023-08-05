826bbadeea90ca7d1dba56b1723ccf1d9ed09775
Rudraksh Karpe <rudraksh.karpe@gmail.com>
2023-08-05 09:01:59 +0530

    feat: DAG create method to generate_product

--- airflow/dags/etl.py
+++ airflow/dags/etl.py
@@ -11,44 +11,41 @@ import traceback
 import sys
 
 with DAG(
-    dag_id='e-commerce_analysis_etl',
+    dag_id="e-commerce_analysis_etl",
     schedule_interval=None,
     catchup=False,
-    start_date=datetime(2023,1,1)
-    ) as dag:
-    
+    start_date=datetime(2023, 1, 1),
+) as dag:
+
     def extract():
-        es = Elasticsearch(['https://192.168.0.56:9200'],
-                            http_auth=('elastic', 'BlnQuVwkGQy09E=S+d*S'),
-                            verify_certs=False
-                        )
+        es = Elasticsearch(
+            ["https://192.168.0.56:9200"],
+            http_auth=("elastic", "BlnQuVwkGQy09E=S+d*S"),
+            verify_certs=False,
+        )
 
         # Specify the index name
-        index_name = "kibana_sample_data_ecommerce" 
+        index_name = "kibana_sample_data_ecommerce"
         # Define a query (optional)
-        query = {
-            "size":10000,
-            "query": {
-                "match_all": {}
-            }
-        }
+        query = {"size": 10000, "query": {"match_all": {}}}
         # Execute the search query
         result = es.search(index=index_name, body=query)
         products = OrderedDict()
         # Process and print the search results
-        for hit in result['hits']['hits']:
-            for product in hit['_source']["products"]:
+        for hit in result["hits"]["hits"]:
+            for product in hit["_source"]["products"]:
                 try:
-                    products[product['product_id']]=product
+                    products[product["product_id"]] = product
                 except:
                     continue
-        prdcts=pd.DataFrame(products).T
-        prdcts.to_csv("/opt/airflow/datasets/products.csv",index=False)
+        prdcts = pd.DataFrame(products).T
+        prdcts.to_csv("/opt/airflow/datasets/products.csv", index=False)
 
-    extract_task=PythonOperator(
-        task_id='extract',
-        python_callable=extract,
-        dag=dag
-    )
+    extract_task = PythonOperator(task_id="extract", python_callable=extract, dag=dag)
 
-    
+    def generate_product(values):
+        product = {}
+        for k, v in values.items():
+            product[k] = random.choice(v)
+        product["product_id"] = -1
+        return product
